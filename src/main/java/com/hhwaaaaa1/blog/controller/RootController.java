8e23b659d630382f899759fabfc9ab02d09c4e92
kim <zlagusdbs89@interpark.com>
2020-01-14 15:33:24 +0900

    Controller initialize

---
+++ src/main/java/com/hhwaaaaa1/blog/controller/RootController.java
@@ -0,0 +1,12 @@
+package com.hhwaaaaa1.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+    @RequestMapping(value = "/")
+    public String welcome(){
+        return "index";
+    }
+}
