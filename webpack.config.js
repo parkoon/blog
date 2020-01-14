6e64690b911b71f93a23b342ff789acff362921c
김화정 <hwajeong@qualson.com>
2020-01-14 12:50:46 +0900

    webpack 설정 변경 - context, devtool, mode 변경 - entry 포인트 변경 - output 경로 변경 - babel env 프리셋 옵션에 타겟 브라우저 추가 - htmlWebpackPlugin 추가
    gitignore 항목 추가
    - webapp/dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,55 @@
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: path.resolve(__dirname, 'src/main/frontend'),
-    entry: {
-        main: './MainPage.jsx'
-    },
-    devtool: 'sourcemaps',
-    cache: true,
-    output: {
-        path: __dirname,
-        filename: './src/main/webapp/js/react/[name].bundle.js'
-    },
-    mode: 'none',
-    module: {
-        rules: [ {
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [ '@babel/preset-env', '@babel/preset-react' ]
-                }
-            }
-        }, {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-        } ]
-    }
+  context: path.resolve(__dirname, 'src/main/views'),
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: {
+    app: './src/app',
+    admin: './src/admin',
+  },
+  output: {
+    path: path.resolve(__dirname, 'src/main/webapp/dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: { browsers: ['> 1% in KR'] } }],
+              '@babel/preset-react',
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Blog',
+      filename: 'app.html',
+      chunks: ['app'],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Admin for blog',
+      filename: 'admin.html',
+      chunks: ['admin'],
+    }),
+  ],
+  devtool: 'eval',
+  cache: true,
+  mode: 'development',
 };
