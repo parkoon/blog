3ee5f2bba8c026e12a8f7fcbb581d480d19d3453
Park JongHyeok <devparkoon@gmail.com>
2020-02-08 09:49:54 +0900

    Update PostList.js
    1. 3항 연산자로 처리하는게 더 깔끔해 보이겠네요.
    2. post && post.map 과 같이 처리하는건 post 가 undefined 일 수 있으니, 예외처리는 잘했음. 개인적으로 리액트에서 제일 더러워 질 수 있는 부분이 render 부분임. 지금은 컴포넌트가 간단해서 위와같은 예외처리가 눈에 들어오지만, 컴포넌트가 여러개 곂치면, 진짜 복잡해짐 (오닥 상황)
    따라서 값을 받아 올 때 초기값을 세팅해주면 더 깔끔한 render 함수를 만들 수 있음.

--- src/main/client/src/components/PostList.js
+++ src/main/client/src/components/PostList.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PostItem from './PostItem';
 
-const PostList = ({ loading, posts }) => {
+const PostList = ({ loading, posts=[] }) => {
   return (
     <>
-      {loading && `Loading ...`}
-      {!loading &&
-        posts &&
-        posts.map(post => <PostItem key={post.id} post={post} />)}
+    {
+      loading ? `Loading ...` : posts.map(post => <PostItem key={post.id} post={post} />)
+    }
     </>
   );
 };
